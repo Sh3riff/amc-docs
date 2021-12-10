89a3e98082ea0fa1476a4b43e0219ae30322303e
sh3riff <contactme@sh3riff.me>
2021-12-10 06:28:53 +0100

    Server Init

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require('path') 
+const express = require('express') 
+const cors = require('cors') 
+const swaggerUi = require('swagger-ui-express') 
+const YAML = require('yamljs')
+
+const swaggerDocs = YAML.load(path.resolve(__dirname, './swagger.yaml'))
+
+const app = express()
+app.use(cors())
+app.use(
+  '/',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocs, { explorer: true })
+)
+app.listen(process.env.PORT || 5000, console.error('server running...'))
